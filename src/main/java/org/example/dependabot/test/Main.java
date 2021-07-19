15bfcbc75577f2d0e8fe763a2e898db05b52500d
Mechetin Artur <Artur.Mechetin@leroymerlin.ru>
2021-07-19 17:28:18 +0300

    add test

---
+++ src/main/java/org/example/dependabot/test/Main.java
@@ -0,0 +1,7 @@
+package org.example.dependabot.test;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
